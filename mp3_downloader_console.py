8c00bcb6a5c6d50d2a9b8a894783d7464d757cbd
yirod3721 <ya.31.01.2008@gmail.com>
2025-09-22 21:25:01 +0300

    Fixed Input Errors and added external file support

--- mp3_downloader_console.py
+++ mp3_downloader_console.py
@@ -1,10 +1,19 @@
 import yt_dlp 
-
-url = input('enter url: ')
-
+choice = str(input("Enter File for file use or cmd for cmd use: "))
+while ((choice != "File") and (choice != "cmd")):
+    print("please enter either file or cmd")
+    choice = str(input("Enter File for file use or cmd for cmd use: "))
+if choice == "File":
+    file_path = str(input("enter appropiate file path: "))
+    with open(file_path, 'r', encoding='utf-8') as f:
+        urls = [line.strip() for line in f if line.strip() and not line.startswith('#')]
+if choice == "cmd":
+    urls = input("enter your urls: ")
+#multiple url download
 ytd_opt_audio = {
     'writethumbnail': True,
     'format': 'bestaudio/best',
+    #cookie input
     'cookiefile': 'cookies.txt',  # <-- add this line
     'postprocessors': [
         {
@@ -19,8 +28,9 @@ ytd_opt_audio = {
         {'key': 'EmbedThumbnail'},
     ]
 }
+#Error Handling
 try:
     with yt_dlp.YoutubeDL(ytd_opt_audio) as ydl:
-        ydl.download([url])
+        ydl.download(urls)
 except yt_dlp.utils.DownloadError as e:
     print("Download Utils", e)
